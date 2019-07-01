6fa8f50b712acd6ca3e1c2133f7be17598e9f4b4
Kevin Riehl <kevinriehl@gmail.com>
2019-06-30 23:55:53 -0700

    Changed rename functionality, added Season number entry, updated GUI

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 import sys
 import re
-from Tkinter import *
-import tkFileDialog
+from tkinter import *
+import tkinter.filedialog
 
 regex = re.compile(r'S\d*E\d*', re.IGNORECASE)
+dir_path = os.path.dirname(os.path.realpath(__file__))
+favicon = os.path.join(dir_path, "favicon.ico")
 folder = ""
 clickCount = 0
 class App:
@@ -12,44 +14,68 @@ class App:
 
 		def getFiles():
 			global folder
-			folder = tkFileDialog.askdirectory(parent=root, initialdir="./", title="Please Select a Directory")
-			addRename()
-			self.folderSelected.delete(0, END)
-			self.folderSelected.insert(0,folder)
+			folder = tkinter.filedialog.askdirectory(parent=root, initialdir="./", title="Please Select a Directory")
+			if (folder == ""):
+				self.folderSelected.delete(0, END)
+				self.folderSelected.insert(0, "No folder selected, try again.")
+				delRename()
+			else:
+				addRename()                        
+				self.folderSelected.delete(0, END)
+				self.folderSelected.insert(0,folder)
 
 
-		def renameFiles(show):
+		def renameFiles(show, season):
+			count = 1
 			for file in os.listdir(folder):
 				extension = "." + file.split(".")[len(file.split("."))-1]
-				matched = regex.findall(file)
-				if matched:
-					season = matched[0]
-					season = season.upper()
-					episodeName = show + " " + season + extension
-					os.rename(os.path.join(folder, file), os.path.join(folder, episodeName))
-			
+				if (count < 10):
+					episode = show + " S0" +str(season) + "E0" + str(count) + extension
+				else:
+					episode = show + " S0" +str(season) + "E" + str(count) + extension
+
+				os.rename(os.path.join(folder, file), os.path.join(folder, episode))
+
+				count+=1
+
+
 			self.folderSelected.delete(0, END)
+			self.show.delete(0, END)
+			self.season.delete(0, END)
+			delRename()
 
 		def addRename():
-			self.rename = Button(master, text="Rename Files", command=lambda:renameFiles(self.show.get())).grid(row=2,column=2)
-			
+			self.rename.config(command=lambda:renameFiles(self.show.get(), self.season.get()), state=ACTIVE, relief=RAISED)
+
+		def delRename():
+			self.rename.config(state=DISABLED, relief=SUNKEN)
+
 		self.input = Label(master, text="Show Name:")
 		self.input.grid(row=0)
-		self.show = Entry(master, width="100")
+		self.show = Entry(master, width="50")
 		self.show.grid(row=0,column=1)
-		
+
+		self.seasonInput = Label(master, text="Season Number:")
+		self.seasonInput.grid(row=1)
+		self.season = Entry(master, width="50")
+		self.season.grid(row=1,column=1)
+
 		self.selectedFolder = Label(master, text="Selected Folder:")
-		self.selectedFolder.grid(row=1)
-		self.folderSelected = Entry(master, width="100")
-		self.folderSelected.grid(row=1,column=1)
-		
+		self.selectedFolder.grid(row=2)
+		self.folderSelected = Entry(master, width="50")
+		self.folderSelected.grid(row=2,column=1)
+
 		self.openFiles = Button(master, text="Select Folder", command=getFiles)
-		self.openFiles.grid(row=2)
+		self.openFiles.grid(row=3)
 		self.quit = Button(master, text="Quit", command=root.destroy)
-		self.quit.grid(row=2,column=1)
+		self.quit.grid(row=3,column=1)
+
+		self.rename = Button(master, text="Rename Files", state=DISABLED, relief=SUNKEN)
+		self.rename.grid(row=0, column=3, columnspan=2, rowspan=4, sticky='WENS')
 
 root = Tk()
-root.geometry("800x70")
+root.geometry("477x89")
+root.iconbitmap(favicon)
 root.title("Rename Show")
 app = App(root)
 
